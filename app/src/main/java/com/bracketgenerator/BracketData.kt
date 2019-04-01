0a20e9dad051503aa76ccd39fdb676b05e67ba81
David.dionise@detroitlabs.com <David.dionise@detroitlabs.com>
2019-04-01 10:31:36 -0400

    add data binding

--- app/src/main/java/com/bracketgenerator/BracketData.kt
+++ app/src/main/java/com/bracketgenerator/BracketData.kt
@@ -1,2 +1,9 @@
 package com.bracketgenerator
 
+import androidx.databinding.ObservableField
+import java.util.*
+
+data class BracketData(
+    var bracketName: ObservableField<String> = ObservableField(""),
+    var players: MutableList<String> = mutableListOf("", "")
+)
--- app/src/main/java/com/bracketgenerator/BracketForm.kt
+++ app/src/main/java/com/bracketgenerator/BracketForm.kt
@@ -3,14 +3,20 @@ package com.bracketgenerator
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bracketgenerator.databinding.FragmentBracketFormBinding
 
 class BracketForm : Fragment() {
     private val data: MutableList<String> = mutableListOf(
@@ -20,20 +26,38 @@ class BracketForm : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: NameInputListAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var binding: FragmentBracketFormBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(
-            R.layout.fragment_bracket_form, container, false
-        ).apply {
-            setOnClickListener {
-                clearFocus()
-                val ims = activity!!.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-                ims.hideSoftInputFromWindow(it.windowToken, 0)
+        binding = DataBindingUtil.inflate<FragmentBracketFormBinding>(
+            inflater,
+            R.layout.fragment_bracket_form,
+            container,
+            false
+        ).also {
+            it.bracket = BracketData()
+            it.root.run {
+                setOnClickListener { view ->
+                    view.clearFocus()
+                    val ims = activity!!.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                    ims.hideSoftInputFromWindow(windowToken, 0)
+                }
+
+                findViewById<EditText>(R.id.bracket_form_input_field).addTextChangedListener(object : TextWatcher {
+                    override fun afterTextChanged(s: Editable?) {}
+
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                        binding.bracket!!.bracketName.set(s.toString())
+                    }
+                })
             }
         }
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
